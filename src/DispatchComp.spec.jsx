f2260911879ac9d9697036698f34ed7896b66ad0
Dominic Schira <DominicS@wsrb.com>
2022-01-19 15:43:19 -0800

    FIxed testing config

--- src/DispatchComp.spec.jsx
+++ src/DispatchComp.spec.jsx
@@ -1,3 +1,5 @@
+import {CountUp} from './Context'
+
 beforeAll(() => {
   console.log("Now Testing The Dispatch Component");
 });
@@ -8,3 +10,12 @@ test.skip("The Dispatch Component", () => {
 
 
 
+test("The Dispatch Component should add", () => {
+  const initState = {
+    count: 0
+  }
+
+  let result = CountUp(initState);
+
+  expect(result).toBe(initState.count);
+});
