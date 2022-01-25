8ef44241564e206df9c041279ace87a2cbb70100
Dominic Schira <DominicS@wsrb.com>
2022-01-25 08:39:50 -0800

    Added more tests

--- src/DispatchComp.spec.jsx
+++ src/DispatchComp.spec.jsx
@@ -1,17 +1,30 @@
-import {CountUp} from './Context'
+import {CountUp, Reducer} from './Context'
+
+let initState = {};
 
 beforeAll(() => {
   console.log("Now Testing The Dispatch Component");
+
+  initState = {
+    count: 0
+  }
 });
 
 
-test("The Dispatch Component should add", () => {
-  const initState = {
-    count: 0
-  }
+test("The Dispatch Component should add when calling CountUp", () => {
+  let expected = initState.count + 1;
 
   let result = CountUp(initState);
-  console.log(initState);
 
-  expect(result).toBe(initState.count);
+  expect(expected).toBe(initState.count);
+  expect(expected).toBe(result);
+});
+
+test("The Dispatch Component should add when called from reducer", () => {
+
+  let expected = initState.count + 1;
+
+  Reducer(initState, {type: "add"});
+
+  expect(expected).toBe(initState.count);
 });
