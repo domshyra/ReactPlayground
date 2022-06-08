7d722b0e45f32c2fc3c12793cbb91dea82fd470e
Dominic Schira <DominicS@wsrb.com>
2022-06-08 15:33:04 -0700

    Added seert

--- WebApp.UITests/CheckBoxItemShould.cs
+++ WebApp.UITests/CheckBoxItemShould.cs
@@ -4,14 +4,26 @@ using Xunit;
 
 namespace WebApp.UITests
 {
+    /// <summary>
+    /// Used to test checboxItems
+    /// </summary>
+    /// <remarks>Make sure the front end webpack server is running</remarks>
     public class CheckBoxItemShould
     {
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
-            using IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://localhost:3000");
+            //Act
+            string expectedTitle = "React App";
+            using IWebDriver chromeDriver = new ChromeDriver();
+
+            //Arrange
+            chromeDriver.Navigate().GoToUrl("http://localhost:3000");
+            string actualTitle = chromeDriver.Title;
+
+            //Assert
+            Assert.Equal(expectedTitle, actualTitle);
         }
 
     }
