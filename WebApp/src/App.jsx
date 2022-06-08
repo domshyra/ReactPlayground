1075ec686dd680e285eb35ea5646164202ce8e7e
Dominic Schira <DominicS@wsrb.com>
2022-06-08 15:41:42 -0700

    Added old code examples back

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -3,14 +3,24 @@ import "./App.css";
 import ReduxComp from "./components/redux/ReduxComp";
 import { Typography } from "@mui/material";
 
+// import DispatchComp from "./DispatchComp";
+// import { Provider } from "./Context";
+
+// import StateComp from "./StateComp";
+
+
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<Typography variant="h4" component="div" gutterBottom pb={2}>
-					Redux Test Application
+					React playground
 				</Typography>
 				<ReduxComp />
+				{/* <StateComp /> used for useState examples */}
+				{/* <Provider> used for useContext examples 
+					<DispatchComp />
+				</Provider>  */}
 			</header>
 		</div>
 	);
